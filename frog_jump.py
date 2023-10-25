da5cff4dcbe0dc201d5050b61695cc7193cc5d25
reshma07mr <reshmamr1005@gmail.com>
2023-10-25 15:33:01 +0200

    Add a file

---
+++ frog_jump.py
@@ -0,0 +1,18 @@
+'''A small frog wants to get to the other side of the road.
+The frog is currently located at position X and wants to get to a
+ position greater than or equal to Y. The small frog always jumps
+ a fixed distance, D.
+Count the minimal number of jumps that the small frog must
+ perform to reach its target.''''
+ 
+ def function():
+    initial = int(input("Enter the initial position"))
+    final = int(input("Enter finalposition"))
+    d = int(input("Enter the distance hopped in one jump"))
+    diff = final - initial
+    diff = int(diff/d)
+    if diff != 0:
+         diff=diff+1
+    return (diff)
+
+print(function())
